bdd71545561a0c72bbab8c58475dc86c5f4f01dc
Jakub-Svoboda <svo.jakub@seznam.cz>
2017-02-20 18:24:21 +0100

    .

--- cha.php
+++ cha.php
@@ -12,12 +12,39 @@ function printHelp(){
 	echo "\n";
 	echo "--help: Prints out the instructions.\n";
 	echo "--input=fileordir: The file or the directory for the analysis. In case of a folder, all the files and subfolders are analysed.\n";
+	echo "--output=filename: Specifies the name of XML file that will be created as an outup. If no file is specified, the table will be printed on standard output.\n";
 	echo "--pretty-xml=k: Formats the XML output file into more readable form. Files in subdirectories get a k-spaces indentation.\n";
 	echo "--no-inline: Skips inline functions.\n";
-	echo "--max-par=l: Only functions with l parameters get processed.\n";
+	echo "--max-par=n: Only functions with l parameters get processed.\n";
+	echo "--no-duplicates: Duplicate function prototypes will be processed only once.\n";
 	echo "--remove-whitespace: Replaces all white spaces with spaces and removes obsolete spaces.\n";
 	echo "\n";
 }
+$longops=array(
+	"help::",
+	"input:",
+	"output:",
+	"pretty-xml::",
+	"no-inline::",
+	"max-par:",
+	"no-duplicates::",
+	"remove-whitespace::"
+);
+
+$flags = getopt(NULL, $longops);
+$flagCount= count($flags);
+for($x = 0; $x < $flagCount; $x++) {
+    echo $flags[$x];
+    echo "\n";
+}
+
+
+//if ($flags == false){
+//	echo "No params\n\n";
+//}else{
+//	echo array_key_exists(0, $flags) == false;
+//	echo "\nhello\n";
+//}
 
 
 ?>
