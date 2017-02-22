b12acb8534c2f75f759bccfa2591f91113bc084d
Jakub-Svoboda <svo.jakub@seznam.cz>
2017-02-22 14:24:22 +0100

    arguments done

--- cha.php
+++ cha.php
@@ -20,6 +20,40 @@ function printHelp(){
 	echo "--remove-whitespace: Replaces all white spaces with spaces and removes obsolete spaces.\n";
 	echo "\n";
 }
+
+function checkDuplicates(array $flags, $parameter){		//TODO
+	if (is_array($flags[$parameter])) {
+			fwrite(STDERR,"Duplicite parameter. \n"  );
+			die(ERROR_PARAMETERS);
+		}
+}
+
+function checkValidArguments(array $argv){
+	$counter =0;
+	foreach($argv as $argument){
+		if($counter == 0){
+			$counter++;
+			continue;
+		}
+		if ((strpos($argument, 'help') === false) 	&& 
+			(strpos($argument, 'input') === false) 	&& 
+			(strpos($argument, 'output') === false) && 
+			(strpos($argument, 'pretty-xml') === false) && 
+			(strpos($argument, 'no-inline') === false)&& 
+			(strpos($argument, 'max-par') === false)&& 
+			(strpos($argument, 'no-duplicate') === false) &&
+			(strpos($argument, 'remove-whitespace') === false))	
+		{		
+			fwrite(STDERR,"Unknown parameter: ");
+			fwrite(STDERR, $argument);
+			fwrite(STDERR, "\n");	
+			exit(ERROR_PARAMETERS);
+		}
+		$counter++;
+	}
+	
+}
+
 $longops=array(
 	"help::",
 	"input:",
@@ -29,22 +63,65 @@ $longops=array(
 	"max-par:",
 	"no-duplicates::",
 	"remove-whitespace::"
+	
 );
+
+define("ERROR_PARAMETERS",1);
+define("ERROR_INPUT",2);
+define("ERROR_OUTPUT",3);
 
 $flags = getopt(NULL, $longops);
 $flagCount= count($flags);
-for($x = 0; $x < $flagCount; $x++) {
-    echo $flags[$x];
-    echo "\n";
+checkValidArguments($argv);								//Makes sure that a nondefined parameter cannot be entered.
+
+//parameters default values:
+$input = false;
+$output = false;
+$prettyXml=4;			//default value
+$noInline = false;
+$maxPar=false;
+$noDuplicates=false;
+$removeWhitespace=false;
+
+
+foreach(array_keys($flags) as $parameter){	
+	checkDuplicates($flags, $parameter);					//Duplicit argument control, aka --input --input		//TODO
+	switch ($parameter) {
+		case "help":										//help parameter recognized
+			if($flagCount !== 1){
+				fwrite(STDERR,"Parameter --help received in combination with another parameter.\n");
+				exit(ERROR_PARAMETERS);
+			}else{
+				printHelp();
+				exit(0);
+			}
+			break;
+		case "input":										//input parameter recognized	
+			$input = $flags[$parameter];
+			break;
+		case "output":										//output parameter recognized
+			$output=$flags[$parameter];
+			break;
+		case "pretty-xml":									//pretty-xml parameter recognized
+			$prettyXml=$flags[$parameter];
+			break;
+		case "no-inline":									//no-inline parameter recognized
+			$noInline=true;
+			break;
+		case "max-par":										//max-par parameter recognized
+			$maxPar=$flags[$parameter];
+			break;
+		case "no-duplicates":								//no-duplicates parameter recognized
+			$noDuplicates=true;
+			break;
+		case "remove-whitespace":							//remove-whitespace parameter recognized
+			$removeWhitespace=true;
+			break;		
+		default:
+			fwrite(STDERR,"Unknown parameter. \n");			//obsolete
+			exit(ERROR_PARAMETERS);
+			
+	}		
 }
-
-
-//if ($flags == false){
-//	echo "No params\n\n";
-//}else{
-//	echo array_key_exists(0, $flags) == false;
-//	echo "\nhello\n";
-//}
-
 
 ?>
